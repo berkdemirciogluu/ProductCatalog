e2a8bcf3b887f519da09a1affbd9893ed3955451
berkdemircioglu <berkdemircioglu@yahoo.com>
2022-09-20 18:56:53 +0300

    .

--- ProductCatalog.DataAccess/NHibernate/Context/NHibernatePostgreSqlContext.cs
+++ ProductCatalog.DataAccess/NHibernate/Context/NHibernatePostgreSqlContext.cs
@@ -10,16 +10,6 @@ namespace ProductCatalog.DataAccess.NHibernate.Context
     public class NHibernatePostgreSqlContext
     {
         private static ISessionFactory _session;
-        public IConfiguration _configuration { get; }
-        private string host;
-
-        public NHibernatePostgreSqlContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            string connectionString = _configuration.GetConnectionString("PostgreSqlConnection");
-
-        }
-
         private static ISessionFactory CreateSession()
         {
             if (_session != null)
@@ -27,8 +17,13 @@ namespace ProductCatalog.DataAccess.NHibernate.Context
                 return _session;
             }
 
-            FluentConfiguration _config = Fluently.Configure().
-                Database(PostgreSQLConfiguration.Standard.ConnectionString(c => c.Host("localhost").Port(5432).Database("productcatalog").Username("postgres").Password("hilal71OKTAY11")))
+            var configuration = GetConfiguration();
+            var conString = configuration.GetConnectionString("PostgreSqlConnection");
+
+            FluentConfiguration _config = Fluently.Configure()
+                .Database(PostgreSQLConfiguration.Standard
+                .ConnectionString(c => c.Is(conString)))
+                //Database(PostgreSQLConfiguration.Standard.ConnectionString(c => c.Host("localhost").Port(5432).Database("productcatalog").Username("postgres").Password("hilal71OKTAY11")))
                 .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
                 .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(true, true)); //false,true
 
@@ -41,5 +36,13 @@ namespace ProductCatalog.DataAccess.NHibernate.Context
         {
             return CreateSession().OpenSession();
         }
+
+        public static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true,reloadOnChange:true);
+            return builder.Build();
+
+        }
+
     }
 }
